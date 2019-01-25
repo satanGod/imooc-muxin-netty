a9632532684a0bd353dfe3f5d2f2ba04942de1bd
jiangxu <347635784@qq.com>
2019-01-25 17:39:08 +0800

    pom

--- src/main/java/com/imooc/netty/ImoocMuxinNettyApplication.java
+++ src/main/java/com/imooc/netty/ImoocMuxinNettyApplication.java
@@ -2,7 +2,9 @@ package com.imooc.netty;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = {"com.imooc.netty"})
 @SpringBootApplication
 public class ImoocMuxinNettyApplication {
 
