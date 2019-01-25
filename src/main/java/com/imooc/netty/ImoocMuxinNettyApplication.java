ee285a0e6392929235a4c1478fd10c497a929ccf
jiangxu <347635784@qq.com>
2019-01-25 17:04:22 +0800

    lx

---
+++ src/main/java/com/imooc/netty/ImoocMuxinNettyApplication.java
@@ -0,0 +1,14 @@
+package com.imooc.netty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImoocMuxinNettyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImoocMuxinNettyApplication.class, args);
+    }
+
+}
+
